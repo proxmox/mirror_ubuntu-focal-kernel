9176058496162048efc9613e7ae0041f00e33c00
Kleber Sacilotto de Souza <kleber.souza@canonical.com>
2021-09-03 17:35:24 +0200

    UBUNTU: [Config] Disable virtualbox dkms build
    BugLink: https://bugs.launchpad.net/bugs/1933248
    
    virtualbox-guest-dkms is not built from virtualbox source anymore,
    users will need to use the in-tree modules.
    
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- debian.master/rules.d/amd64.mk
+++ debian.master/rules.d/amd64.mk
@@ -22,6 +22,6 @@ do_extras_package = true
 do_tools_common = true
 do_tools_acpidbg = true
 do_zfs		= true
-do_dkms_vbox    = true
+do_dkms_vbox    = false
 do_dkms_wireguard = true
 do_odm_drivers    = true
